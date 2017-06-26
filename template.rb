5592c48009acfc9eba881dd8105bed244716ba12
longh957 <longh957@gmail.com>
2017-06-26 16:03:51 -0700

    updates template for prompts (#1)
    * updates template for prompts
    
    * add .ruby-version file

--- template.rb
+++ template.rb
@@ -4,13 +4,22 @@ def source_paths
     Array(super)
 end
 
+# Adds Ruby Version to gemfile
+insert_into_file('Gemfile', "ruby '#{RUBY_VERSION}'\n", before: /^ *gem 'rails'/, force: false)
+
+# Template Gems
 gem 'slim-rails'
+gem 'high_voltage'
+
+insert_into_file('Gemfile', '# Template Gems', before: /^ gem 'slim-rails'/, force: false)
+
+# Rack Gems
 gem 'stackprof'
 gem 'rack-mini-profiler'
 gem 'flamegraph'
-gem 'high_voltage'
 gem 'rack-timeout'
 
+# Testing Gems
 gem_group :development, :test do
   gem 'factory_girl_rails'
   gem 'faker'
@@ -24,20 +33,36 @@ gem_group :test do
   gem 'poltergeist'
   gem 'shoulda'
   gem 'capybara'
-  # gem 'capybara-webkit'
   gem 'timecop'
 end
 
+# Development Gems
 gem_group :development do
   gem 'spirit_fingers'
   gem 'better_errors'
   gem 'meta_request'
   gem 'pry-toys'
-  # use bullet for DB
-  # gem 'bullet'
   gem 'guard-livereload'
   gem 'terminal-notifier-guard'
 end
+
+# Caching
+gem 'redis'
+gem 'redis-rails'
+gem 'redis-namespace'
+gem 'redis-store'
+gem 'actionpack-action_caching'
+
+# Devise
+if yes?('Use Devise for Auth?')
+  gem 'devise'
+  generate 'devise:install'
+  model_name = ask('What would you like to call the Devise Model? [user]')
+  model_name = 'user' if model_name.blank?
+  generate 'devise', model_name
+end
+
+insert_into_file('Gemfile', '# Caching', before: /^ gem 'redis'/, force: false)
 
 after_bundle do
   # remove standard genearted files
@@ -68,8 +93,10 @@ after_bundle do
   copy_file '.foreman'
   copy_file '.eslintrc'
   copy_file '.editorconfig'
-  copy_file '.rubocop'
+  copy_file '.rubocop.yml'
+  copy_file '.ruby-version'
 
+  append_file '.ruby-version', RUBY_VERSION.to_s
 
   run 'yarn add slm-loader'
   run 'yarn add eslint eslint-config-airbnb --dev'
